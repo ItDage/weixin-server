947b09fd92691f58c102bd687db3b15fe170640e
ItDage <scy131164264@163.com>
2018-06-19 16:51:33 +0800

    解密算法

--- src/main/java/com/cap/util/AES.java
+++ src/main/java/com/cap/util/AES.java
@@ -3,6 +3,7 @@ package com.cap.util;
 /**
  * Created by scy on 2018/6/19.
  */
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.security.AlgorithmParameters;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -10,14 +11,12 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.Security;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
  * AES加密
@@ -26,25 +25,27 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
  */
 public class AES {
 
-    public static boolean initialized = false;
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     /**
      * AES解密
-     *
-     * @param content
-     *            密文
+     * @param content 密文
      * @return
      * @throws InvalidAlgorithmParameterException
      * @throws NoSuchProviderException
      */
-    public byte[] decrypt(byte[] content, byte[] keyByte, byte[] ivByte) throws InvalidAlgorithmParameterException {
-        initialize();
+    public static byte[] decrypt(byte[] content, byte[] keyByte, byte[] ivByte)
+            throws InvalidAlgorithmParameterException {
         try {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
             Key sKeySpec = new SecretKeySpec(keyByte, "AES");
-            cipher.init(Cipher.DECRYPT_MODE, sKeySpec, generateIV(ivByte));// 初始化
-            byte[] result = cipher.doFinal(content);
-            return result;
+            //生成iv
+            AlgorithmParameters params = AlgorithmParameters.getInstance("AES");
+            params.init(new IvParameterSpec(ivByte));
+            cipher.init(Cipher.DECRYPT_MODE, sKeySpec, params);// 初始化
+            return cipher.doFinal(content);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (NoSuchPaddingException e) {
@@ -55,27 +56,9 @@ public class AES {
             e.printStackTrace();
         } catch (BadPaddingException e) {
             e.printStackTrace();
-        } catch (NoSuchProviderException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return null;
-    }
-
-    public static void initialize() {
-        if (initialized)
-            return;
-        Security.addProvider(new BouncyCastleProvider());
-        initialized = true;
-    }
-
-    // 生成iv
-    public static AlgorithmParameters generateIV(byte[] iv) throws Exception {
-        AlgorithmParameters params = AlgorithmParameters.getInstance("AES");
-        params.init(new IvParameterSpec(iv));
-        return params;
     }
 }
--- src/main/java/com/cap/util/WXCore.java
+++ src/main/java/com/cap/util/WXCore.java
@@ -3,15 +3,17 @@ package com.cap.util;
 /**
  * Created by cmhy on 2018/6/19.
  */
-import org.apache.commons.codec.binary.Base64;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+
 
 import net.sf.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
 
 
 /**
  * 封装对外访问方法
- * @author liuyazhuang
- *
  */
 public class WXCore {
 
@@ -22,32 +24,73 @@ public class WXCore {
      * @return
      * @throws Exception
      */
-    public static String decrypt(String appId, String encryptedData, String sessionKey, String iv){
-        String result = "";
+    public static String illegalAesKey = "-41001";//非法密钥
+    public static String illegalIv = "-41002";//非法初始向量
+    public static String illegalBuffer = "-41003";//非法密文
+    public static String decodeBase64Error = "-41004"; //解码错误
+    public static String noData = "-41005"; //数据不正确
+
+    private String appid;
+
+    private String sessionKey;
+
+    public WXCore(String appid, String sessionKey) {
+        this.appid = appid;
+        this.sessionKey = sessionKey;
+    }
+
+    /**
+     * 检验数据的真实性，并且获取解密后的明文.
+     * @param encryptedData  string 加密的用户数据
+     * @param iv  string 与用户数据一同返回的初始向量
+     * @return data string 解密后的原文
+     * @return String 返回用户信息
+     */
+    public String decryptData(String encryptedData, String iv) {
+        if (StringUtils.length(sessionKey) != 24) {
+            return illegalAesKey;
+        }
+        // 对称解密秘钥 aeskey = Base64_Decode(session_key), aeskey 是16字节。
+        byte[] aesKey = Base64.decodeBase64(sessionKey);
+
+        if (StringUtils.length(iv) != 24) {
+            return illegalIv;
+        }
+        // 对称解密算法初始向量 为Base64_Decode(iv)，其中iv由数据接口返回。
+        byte[] aesIV = Base64.decodeBase64(iv);
+
+        // 对称解密的目标密文为 Base64_Decode(encryptedData)
+        byte[] aesCipher = Base64.decodeBase64(encryptedData);
+
         try {
-            AES aes = new AES();
-            byte[] resultByte = aes.decrypt(Base64.decodeBase64(encryptedData), Base64.decodeBase64(sessionKey), Base64.decodeBase64(iv));
-            if(null != resultByte && resultByte.length > 0){
-                result = new String(WxPKCS7Encoder.decode(resultByte));
-                JSONObject jsonObject = JSONObject.fromObject(result);
-                String decryptAppid = jsonObject.getJSONObject(WATERMARK).getString(APPID);
-                if(!appId.equals(decryptAppid)){
-                    result = "";
+            byte[] resultByte = AES.decrypt(aesCipher, aesKey, aesIV);
+            if (null != resultByte && resultByte.length > 0) {
+                String userInfo = new String(resultByte, "UTF-8");
+                JSONObject jsons = JSONObject.fromObject(userInfo);
+                String id = jsons.getJSONObject("watermark").getString("appid");
+                if (!StringUtils.equals(id, appid)) {
+                    return illegalBuffer;
                 }
+                return userInfo;
+            } else {
+                return noData;
             }
-        } catch (Exception e) {
-            result = "";
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return result;
+        return null;
     }
 
 
-//    public static void main(String[] args) throws Exception{
-//        String appId = "wxe88fcf6c1f564e06";
-//        String encryptedData = "ipbTtaQnIMV2zjreSAaBbyjgw1QKMzE6nXlZT8UKqsYq8w/fUvjVwIj8bYA2OuWpkefukvTzRvu0j3Ptn9jhHOOMZ0VPC9PQpsL0S58R1EZyuksMBdJgDiZX7vQCf2Yf3NwkOVG3vXISZVb4Do9FbxlD+dbROy5aeCVgrBj6z9EcOvugQ+PNUR2tWjAezswP7VOPAEOvR22DUxHqKa+6QM71grEzU3Qk7uA53ZuNeZsaz3FrOEnVZqCjyzEbLAenhJ+WqfezhnhelDJsO3B2kHQtgRtqeG11goBFko5kYX+oKzSUCoxCRvMXDBP7jSmrImTZuiFayzE+w+4by3xXT2iV9ixcGRCFE9M6Rs9jWU8nsGtzvDEM1FofiwQsyGW3G51rbBtAeuphX1XUxT/RTO9Tuvqb3srshNhQZA75jAMzCelVCwLQ4e8dlQurAndrpyhh+v1Z1NovQkopXVHOkp3ndT1eHV1+pP2yk2uG7JkcpLz7rZ/f3Um1BxeF3QKvcGMltmZzCeiBTl6EgHTREQnr1wE305pW49GHWF5qOWBpj2XF1497xJHThu7IMmEh2yggH5CunpgWuvrdbPbOohV/MTw+Hi9ANrjYYPp8Oj3bFLW4/L/oy/qJiFO2cN8sCikJVCsjul71fTJ8djTFH0IvyT9TyR+GPqSnS4vEuKqlPI+ALbeW8RCVAC/nN/Jw7GStJKjDM6UIQ1/4mDUHLUUNnViXcMGhtZB99gDwJ7VoZcjB4Ml63m2D3IVNo4DfEj9xceKgrYtjCNNzYjsFBAlUWVkVGLKRtorxaWlvIoS++NkjonOAJzMyIyKbuZMRg/aYAJcXRIKntl8E3toOpr7qq9vhvkUfKwQTiejDMy6KeAxHEZi16GjU+jZbCbQJ812zWFcbyBfwpPbtxu6rL4+k04q4wP7+RFi70ZWwHCfvxxHb5OVndT7LW3GmxiYdDJqCs0A5pvxTxmi4TFKAvKjg7nm7A7t66wNrYCvH/xHpoJfFquH8+FX+5QqAFl+9pAkrp2OP7IW1pdLzZjhg4vYlqeeOJoYet/d5PZ2fFw3jAi4ckFsOGDTieB45vpAkLybDPZdfLdTgfRlL3gj0USm4iH0PK+roS1B9Io3S7lXTv3OkAsFPdbOFXfg/0BlpF1UqWHqWF2LNf2OfEg02g46VS93WbFxL+3MIagEHnFalNKCpRcGBQUQfWDz3ARx3q0bYcEbLQ+bnZjTcvQfB7Lh0qKlihgqugPXt2tJ9DJvTMLY7oTiyV4ePswa23F8CTpDTvnfnA4tulPL4SL7qWmDMQsvrvreNJ9jmLTtoaUxr4jdHFqw7L5ohIV330k6dAh1SO/GmbpKmjWc3+aBHJyhI5FZlGQkNFsio6uH//rBxNn11f6Sb6F9H4zz9GlHRGK8U0ZvlKfmZ0XPz5SxcplH+ctDJtWgqeumtXUiC3DTQxwm8GMQ/62ud2IC+TY9hC+5VIVhkBgpbdb4w0qHQrvLCdE0QBz4dK6bZ4IbUW49uG6ZH5+OBMm9Rq+N2UVG1YwiElPN5AZLxRMPSny4jOxiaygZ6/l7meWSfZXYu8JUJFop5KK2CCPg2WPqEbrpBk2EyF2387ruAIh44VVN5Xak/YSVN/uWV0zHOikrFifESf5BZ3rWrwVi8ha9Fyzsd";
-//        String sessionKey = "tiihtNczf5v6AKRyjwEUhQ==";
-//        String iv = "cJbXseN846GyEs+MHfFx/w==";
-//        System.out.println(decrypt(appId, encryptedData, sessionKey, iv));
-//    }
+
+    public static void main(String[] args) throws Exception{
+        String appId = "wx4f4bc4dec97d474b";
+        String encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew==";
+        String sessionKey = "tiihtNczf5v6AKRyjwEUhQ==";
+        String iv = "r7BXXKkLb8qrSNn05n0qiA==";
+        WXCore biz = new WXCore(appId, sessionKey);
+        System.out.println(biz.decryptData(encryptedData, iv));
+    }
 }
