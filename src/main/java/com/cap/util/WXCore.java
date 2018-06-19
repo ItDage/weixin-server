e22147b4fa5ed2f9a58cea290a4caa417b03af32
ItDage <scy131164264@163.com>
2018-06-19 19:57:13 +0800

    提交

--- src/main/java/com/cap/util/WXCore.java
+++ src/main/java/com/cap/util/WXCore.java
@@ -5,6 +5,8 @@ package com.cap.util;
  */
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 import net.sf.json.JSONObject;
@@ -86,11 +88,15 @@ public class WXCore {
 
 
     public static void main(String[] args) throws Exception{
-        String appId = "wx4f4bc4dec97d474b";
-        String encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew==";
-        String sessionKey = "tiihtNczf5v6AKRyjwEUhQ==";
-        String iv = "r7BXXKkLb8qrSNn05n0qiA==";
+        String appId = "wxe88fcf6c1f564e06";
+        String encryptedData = "gumAFJwo1uO4iGBW5S2auXQmrfI1hex3UXTJaIda0arRpRKHmuCkx4rccY6EyKJMVqwYBbxfMCioda6om3iOyp/NYhf7bEM3yt/WTCphZNKRcUe344dF4NZ5e4vVQn647NNk5EGlBPm/+myUSPYWhHqTEhPka2If0NlipmA1JQ73Ao3/sDqpLycchFg4Fb53YIgE3xkSzuIxUvw/ndzbsTCeSwHH/f2O3QN5I46E4cD03uoN3//W4e/qCsIX54DcMatM5rJ6aCXu0ycsdnUiV9N1Ltg6Hv3cv5RCBR6yQIIAaLcdnFUVoZAXDtx61Bqxg7H3bjfuD6BkxmGLI/6Z88ZEsd+F3M5+xMKnJZuS1xiImWW9gsb9Zu9gg3MREoR4QBF/VbbUPB/dwxhFipDVg8QPB5aaMxylz4J9HmUnlC5GIzlaDZIQQz4mdVwIY/H6vPzi1AK7JMZvoh28uBqwQJqnmSmP4VoYycUlEnD3uLK0Opa2p5yY5PPT9oYyQAa+tzNVi0I+pO/XfxQO4OXewTK6VzgrSmbINiL2CtD9wQipJwrIbhSKnMHPjuWdVND3JZk7Kg9i/gjDgz1ZwfK0JfZ4Ml+2cMvlY6+D1xZX/yvD17E+ZiBFJi7j7QFqjcrHf6E0AdFdiEItB4lmodulNdIooW/MveG4CEJSYeECg/pXUs+qqwylKNl+I8j8zTOhfjL2W/YSMIcSiX2Lpx+4Ov3W89J+u0zdbZybczSllWpnBrXtdDI1U5aG3myhxhR8i9b0BIiQXKNDYuJvssNwOqiAZAy7sFKcU51NKbPQw7UUaJWUZhKulNw2Rg065mqbr6rSeEXnZDm3RtU0Dx+29o5SuuvKY7vwa3PoYxgCFutvWw4LJStF3iPSr2An440fvNwOVHohOnO+PJcGXYCMc3bC0QVquBuzIf4XlW7vPAKW+DNt4IayRfMmoQKaNQRdgY4qh75xH2DjQVyPgiETz0uKr5NzDl3EDeomhbxjIhXyJ5QdKLFD2jEn/3Y2fmYg82DXfTICEdzITzrNV8KU5bXQhirF01UU7sCCa29JoqxokfRGOLSSpwiXeGdfEL8dZPRqtuP6oik4bIo+K3azG4b+cq3bBZGCWliUhHTCstuDrfB/H+mGwK1rQav6OSMLq4sZyFCjGGnHNtjLV8/RrTz1dUY6kw4VuZbgKg6VlQoVe3Y29epyWAphfDuzUSGE80OEacymUYqeXl0T1ncvfbZyJvJSfhszIjdvC7/B/SlU0P3+/xMrfvaLmiiGbWzGJ3X2yjLqV4q+eO/T12jFFG0ogimbY2voSmA5kRVwnZ0371I6rqPiWDjkvPRX6byObz81HCTUvLndR3KkshOTlfCYTHj4kelhLwU3P/EYpgdCVjRvNtxuGRwOrMbPVt2EF8TEBXlwsLSGGrfgBnOWU7zuGrZsMIzuPlkipANhaJTUNufWYnPicq+73I9W/K8rFcUxVWCMYZnjJ3VTs9b9/+X/fuTQ6MuIfB4i19uPx2BOd3QtcK2wFXTwiyg5NupL53glm+Km++YplmAyVsBsY53ud1JoAA+PsG9h0Xktf/eiE3AqSPfL5GNyBkYR1XnkeJapUdLTMxkiT8Cn+F/InFcixbGMM6r0/LwKnl0SXjxKZBQxI6cwiPC+c4BDC7bN";
+        String sessionKey = "Kg1QRHjT+LHpGQG74RE6aA==";
+        String iv = "RmHsdtr/FxKvnF/8addeHw==";
         WXCore biz = new WXCore(appId, sessionKey);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String s = format.format(new Date(Long.parseLong(String.valueOf("1529251200000"))));
+        System.out.println(s);
+
         System.out.println(biz.decryptData(encryptedData, iv));
     }
 }
