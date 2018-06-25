f7f15844f009a69bfc9acd41429d489d03aeb9a4
ItDage <scy131164264@163.com>
2018-06-25 17:17:46 +0800

    测试

--- src/main/java/com/cap/controller/IndexController.java
+++ src/main/java/com/cap/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Created by cmhy on 2018/6/25.
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/testIndex")
-    public String testIndex(){
-        System.out.println("找到");
+    public String testIndex(RedirectAttributes redirectAttributes){
         return "index";
     }
 }
